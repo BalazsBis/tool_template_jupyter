0052858f96a22e9ffd6f0468951fa51b43f82684
AlexDo1 <alex.dolich@gmail.com>
2022-11-10 11:30:07 +0100

    run.py print() instead of f.write()

--- src/run.py
+++ src/run.py
@@ -1,5 +1,6 @@
 import os
 from datetime import datetime as dt
+from pprint import pprint
 
 from json2args import get_parameter
 
@@ -12,10 +13,11 @@ toolname = os.environ.get('TOOL_RUN', 'foobar').lower()
 # switch the tool
 if toolname == 'foobar':
     # RUN the tool here and create the output in /out
-    with open('/out/STDOUT.log', 'w') as f:
-        f.write('This toolbox does not include any tool. Did you run the template?\n')
+    print('This toolbox does not include any tool. Did you run the template?\n')
+    
+    # write parameters to STDOUT.log
+    pprint(kwargs)
 
 # In any other case, it was not clear which tool to run
 else:
-    with open('/out/error.log', 'w') as f:
-        f.write(f"[{dt.now().isocalendar()}] Either no TOOL_RUN environment variable available, or '{toolname}' is not valid.\n")
+    raise AttributeError(f"[{dt.now().isocalendar()}] Either no TOOL_RUN environment variable available, or '{toolname}' is not valid.\n")
